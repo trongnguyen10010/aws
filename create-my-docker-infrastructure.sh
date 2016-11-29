e124723124384fe9efee274756fb3648ab953755
trong <trongnguyen10010@users.noreply.github.com>
2016-11-28 20:31:12 -0500

    tn

--- create-my-docker-infrastructure.sh
+++ create-my-docker-infrastructure.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mynetwork="burn-dis-bridge4"
-db_vm="db4"
-web_vm="web4"
+mynetwork="burn-dis-bridge4" &&
+db_vm="db4" &&
+web_vm="web4" &&
 
-docker network create -d bridge $mynetwork
+docker network create -d bridge $mynetwork &&
 
 #learning docker
 #https://docs.docker.com/engine/tutorials/networkingcontainers/
 
-docker run -d --network=$mynetwork --name $db_vm training/postgres
-docker run -d --network=$mynetwork --name $web_vm training/webapp python app.py
+docker run -d --network=$mynetwork --name $db_vm training/postgres &&
+docker run -d --network=$mynetwork --name $web_vm training/webapp python app.py &&
 
 docker network inspect $mynetwork
