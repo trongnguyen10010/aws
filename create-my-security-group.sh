dcdde028361642786b9431235e2f738f27c3c61a
trong <trongnguyen10010@users.noreply.github.com>
2016-11-30 02:54:30 -0500

    Update create-my-security-group.sh

--- create-my-security-group.sh
+++ create-my-security-group.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-gname="trump-wall"
+gname="trump-wall3"
 gdes="we got the fire"
 
 aws ec2 create-security-group --group-name $gname --description $gdes
+
+# open inbound ssh
+aws ec2 authorize-security-group-ingress --group-name $gname --protocol 22 --cidr 24.0.203.142/24
+
+#http://docs.aws.amazon.com/cli/latest/userguide/tutorial-ec2-ubuntu.html
+#step 2-2
