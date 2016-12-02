f7a271fae22ce43c839a94384661e0dec743d8ed
trong <trongnguyen10010@users.noreply.github.com>
2016-12-01 19:24:35 -0500

    Update run-instances-ubuntu.sh

--- run-instances-ubuntu.sh
+++ run-instances-ubuntu.sh
@@ -1,8 +1,8 @@
 # AWS ubuntu 16.04 - FREE tier
-key="xxxx" #not xxx.pem
-security="sg-0350677a" # security group id
+# (HVM), SSD Volume Type
 
-aws ec2 run-instances --image-id ami-a9d276c9 \ 
-                      --count 1 --instance-type t2.micro \ 
-                      --key-name $key \
-                      --security-group-ids $security
+key="xxxx" #not xxxx.pem
+security="sg-0350677a" # security group id
+aws ec2 run-instances --image-id ami-a9d276c9 \
+--count 1 --instance-type t2.micro \
+--key-name $key --security-group-ids $security
